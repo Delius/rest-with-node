a485ab6fe0241be30e44372c2b60d5800455d393
peel3r <p@weljakubowski.com>
2015-05-09 12:48:49 +0100

    add post method to give possibility to insert conditions to the database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/conditionAPI');
 
@@ -8,9 +9,17 @@ var  Condition = require('./models/conditionModel');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 var conditionRouter = express.Router();
 
 conditionRouter.route('/Conditions')
+    .post(function(req,res){
+        var condition  = new Condition(req.body);
+        console.log(condition);
+        res.send(condition);
+    })
     .get(function(req,res){
         var query = {};
 
